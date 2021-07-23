9bfa9a8450398d4393c8f8808f5ecd5dad629745
Nenad Stoisavljevic <nenad@nenadstoisavljevic.xyz>
2021-07-22 23:10:53 -0400

    fix checking for multiple files

--- tag.sh
+++ tag.sh
@@ -2,7 +2,7 @@
 
 # Requires Whitacker's Words (Latin dictionary), you can find it at https://github.com/mk270/whitakers-words
 
-letters="ADJ:A
+subs="ADJ:A
 ADV:D
 CONJ:C
 INTERJ:I
@@ -14,30 +14,25 @@ VPAR:R"
 
 sub() {
     for x in $1; do
-        tag=$(echo "$letters" | grep -w "$x" | cut -d ':' -f 2)
-        [ -z "$tag" ] && labels="${labels}$x" || labels="${labels}$tag"
+        letter=$(echo "$subs" | grep -w "$x" | cut -d ':' -f 2)
+        [ -z "$letter" ] && labels="${labels}$x" || labels="${labels}$letter"
     done; printf "%s\n" "$labels"
 }
 
-main() {
+tag() {
     while read -r line; do
         last=${line##* }
         for word in $line; do
             pos=$(words "$word" | grep -Ev '(;|]|words)' | awk '{print $2}' | sort -u)
-            letter=$(sub "$pos")
-            [ "$word" = "$last" ] && printf "%s//%s\n" "$word" "$letter" >> "$output" || printf "%s//%s " "$word" "$letter" >> "$output"
+            letters=$(sub "$pos")
+            [ "$word" = "$last" ] && printf "%s//%s\n" "$word" "$letters" >> "$output" || printf "%s//%s " "$word" "$letters" >> "$output"
         done
     done < "$1"
 }
 
-if [ $# -eq 0 ]
-then
-    echo "Provide file(s) to tag." && exit 1
-else
-    for name in "$@"; do
-        file="$name"
-        output="$file-tagged"
-        echo "Tagging \"$file\"..."
-        main "$file"
-    done
-fi
+[ $# -eq 0 ] && echo "Provide file(s) to tag." && exit 1
+for name in "$@"; do
+    file="$name"
+    output="$name-tagged"
+    echo "Tagging \"$file\"..." && tag "$file"
+done
