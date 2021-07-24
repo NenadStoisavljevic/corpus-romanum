b295302cdcf5493988db289919748092fa5760ba
Nenad Stoisavljevic <nenad@nenadstoisavljevic.xyz>
2021-07-23 22:11:33 -0400

    add comments

--- tag.sh
+++ tag.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Requires Whitacker's Words (Latin dictionary), you can find it at https://github.com/mk270/whitakers-words
+# Requires Whitacker's Words (Latin-English dictionary), you can
+# find it at https://github.com/mk270/whitakers-words.
 
+# A list of substitutes for the parts of speech.
 subs="ADJ:A
 ADV:D
 CONJ:C
@@ -12,24 +14,31 @@ PRON:O
 SUFFIX:S
 VPAR:R"
 
-sub() {
+sub() { # Replace parts of speech with a letter.
     for x in $1; do
         letter=$(echo "$subs" | grep -w "$x" | cut -d ':' -f 2)
         [ -z "$letter" ] && labels="${labels}$x" || labels="${labels}$letter"
     done; printf "%s\n" "$labels"
 }
 
-tag() {
+tag() { # Tag file.
     while read -r line; do
         last=${line##* }
         for word in $line; do
+            # Run the word in `words` and only select the possible
+            # parts of speech.
             pos=$(words "$word" | grep -Ev '(;|]|words)' | awk '{print $2}' | sort -u)
             letters=$(sub "$pos")
+            # Append each tagged word to the output file.
+            # Print a newline when the last word of a sentence is
+            # reached, otherwise print the word with a space.
             [ "$word" = "$last" ] && printf "%s//%s\n" "$word" "$letters" >> "$output" || printf "%s//%s " "$word" "$letters" >> "$output"
         done
     done < "$1"
 }
 
+# Check for number of files provided and tag each one
+# accordingly, otherwise prompt user to provide a file.
 [ $# -eq 0 ] && echo "Provide file(s) to tag." && exit 1
 for name in "$@"; do
     file="$name"
